db08ed6afa270177a0c3d930564b32b3ebe861f8
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-07-08 10:24:50 +0300

    Added categories page and completed entire application

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   root 'splash#index'
-
   devise_for :users
-  resources :group_categories
-  resources :categories
-  resources :groups
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :categories, only: [:index, :show, :create, :destroy, :new] do
+    resources :group, only: [:index, :create, :destroy, :new]
+  end
 end
